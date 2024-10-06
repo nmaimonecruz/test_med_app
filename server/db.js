5975fd9306b203040f13417086dab039f5e2b9b4
nmaimonecruz <nmaimonecruz@gmail.com>
2024-10-06 11:16:17 -0400

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<zKKBGawXmX3Dq6BkQm1qUs5D>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:<zKKBGawXmX3Dq6BkQm1qUs5D>@172.21.105.164:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
