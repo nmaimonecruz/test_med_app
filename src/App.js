05701075ee08b6e4669046b12f2601d891cefcb7
nmaimonecruz <nmaimonecruz@gmail.com>
2024-10-21 14:27:30 -0400

    second commit module 3

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@
 // Import components for routing from react-router-dom library
 // Import custom Navbar component
 import Navbar from './Components/Navbar/Navbar';
-import './App.css';
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing_Page from './Components/Landing_Page/Landing_Page';
+import Login from './Components/Login/Login';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
 
 function App() {
 
@@ -14,10 +16,12 @@ function App() {
               <Navbar/>
                   <Routes>
                     <Route path="/" element={<Landing_Page/>}/>
+                    <Route path="/signup" element={<Sign_Up/>}/>
+                    <Route path="/login" element={<Login/>}/>
                     </Routes>
         </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+export default App
