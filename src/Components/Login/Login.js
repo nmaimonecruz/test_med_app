f7ea8dd7b583d6ba6c6819bdf8c5b408c00c0684
nmaimonecruz <nmaimonecruz@gmail.com>
2024-12-01 18:18:43 -0500

    Update Login.js ES6 syntax

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -2,39 +2,39 @@ import "./Login.css";
 
 function Login() {
     return (
-        <div class="container">
-        <div class="login-grid">
-        <div class="login-text">
+        <div className="container">
+        <div className="login-grid">
+        <div className="login-text">
             <h2>Login</h2>
           </div>
-        <div class="login-text">
+        <div className="login-text">
             Not a memeber yet? <span><a href="../Sign_Up/Sign_Up.html" style="color: #2190FF;"> Sign Up Here</a></span>
         </div>
           <br />
-        <div class="login-form">
+        <div className="login-form">
             <form>
-              <div class="form-group">
+              <div className="form-group">
                 <label for="email">Email</label>
-                <input type="email" name="email" id="email" class="form-control" placeholder="ex:youremail@email.com" aria-describedby="helpId" />
+                <input type="email" name="email" id="email" className="form-control" placeholder="ex:youremail@email.com" aria-describedby="helpId" />
               </div>
-              <div class="form-group">
+              <div className="form-group">
                 <label for="password">Password</label>
                 <input
                   type="password"
                   name="password"
                   id="password"
-                  class="form-control"
+                  className="form-control"
                   placeholder="Enter your password"
                   aria-describedby="helpId"
                 />
               </div>
-              <div class="btn-group">
-                <button type="submit" class="btn btn-primary mb-2 mr-1 waves-effect waves-light">Login</button> 
+              <div className="btn-group">
+                <button type="submit" className="btn btn-primary mb-2 mr-1 waves-effect waves-light">Login</button> 
                 <br> </br>
-                <button type="reset" class="btn btn-danger mb-2 waves-effect waves-light">Reset</button>
+                <button type="reset" className="btn btn-danger mb-2 waves-effect waves-light">Reset</button>
               </div>
               <br />
-              <div class="login-text">
+              <div className="login-text">
                 Forgot Password? <span><a href="../Forgot_Password/Forgot_Password.html" style="color: #2190FF;"> Click Here</a></span>
               </div>
             </form>
@@ -44,4 +44,4 @@ function Login() {
     )
 };
 
-export default Login
+export default Login;
