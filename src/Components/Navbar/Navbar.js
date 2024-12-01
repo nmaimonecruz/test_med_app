2945241a0d16367b1720b0b87ba29f7bc7bd2c5c
nmaimonecruz <nmaimonecruz@gmail.com>
2024-12-01 18:15:44 -0500

    Update to ES6 syntax

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,37 +3,37 @@ import "./Navbar.css";
 function Navbar() {
     return (
     <nav>
-        <div class="nav__logo">
+        <div className="nav__logo">
           <a href="/">
             StayHealthy Inc.
           </a>
           <span>.</span>
         </div>
-        <div class="nav__icon" onClick={"handleClick"}>
-          <i class="fa fa-times fa fa-bars"></i>
+        <div className="nav__icon" onClick={"handleClick"}>
+          <i className="fa fa-times fa fa-bars"></i>
         </div>
 
-        <ul class="nav__links active">
-          <li class="link">
+        <ul className="nav__links active">
+          <li className="link">
             <a href="../Landing_Page/LandingPage.html">Home</a>
           </li>
-          <li class="link">
+          <li className="link">
             <a href="#">Health Blog</a>
           </li>
-          <li class="link">
+          <li className="link">
             <a href="#">Appointments</a>
           </li>
-          <li class="link">
+          <li className="link">
             <a href="#">Reviews</a>
           </li>
-          <li class="link">
+          <li className="link">
             <a href="../Sign_Up/Sign_Up.html">
-              <button class="btn1">Sign Up</button>
+              <button className="btn1">Sign Up</button>
             </a>
           </li>
-          <li class="link">
+          <li className="link">
             <a href="../Login/Login.html">
-              <button class="btn1">Login</button>
+              <button className="btn1">Login</button>
             </a>
           </li>
         </ul>
@@ -41,4 +41,4 @@ function Navbar() {
     );
 }
 
-export default Navbar
+export default Navbar;
