73723e44d9452f0565cde7725cfb2dcc5342deba
nmaimonecruz <nmaimonecruz@gmail.com>
2024-12-04 15:03:39 -0500

    Update App.js routes/links

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@
 // Import custom Navbar component
 import Navbar from './Navbar.js';
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Landing_Page from './Landing_Page.js';
 import Login from './Login.js';
 import Sign_Up from './Sign_Up.js';
 
 function App() {
-    return (
-        <div className="App">
-            <BrowserRouter>
-              <Navbar />
-                  <Routes>
-                    <Route path="/" element={<Landing_Page/>}/>
-                    <Route path="/signup" element={<Sign_Up/>}/>
-                    <Route path="/login" element={<Login/>}/>
-                    </Routes>
-        </BrowserRouter>
+  return (
+    <div> 
+	  <nav>
+      <Link to="/" className="nav-item">Landing_Page</Link>
+        <Link to="/about" className="nav-item">Sign_Up</Link>
+        <Link to="/contact" className="nav-item">Login</Link>
+	  </nav>
+      <Routes> 
+        <Route path="/" element={Landing_Page />}></Route>
+        <Route path="/signup" element={<Sign_Up />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
     </div>
   );
-}
+};
 
-export default App
+export default App;
